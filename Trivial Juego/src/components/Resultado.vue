d7b4f54e171cc8db0ed934a26a83d28fb6d6fc62
C413bxD <caleb.895.9@gmail.com>
2023-08-11 19:23:06 -0500

    interfaz resultado

--- Trivial Juego/src/components/Resultado.vue
+++ Trivial Juego/src/components/Resultado.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    hola
-  </div>
+  <q-page class="flex flex-center">
+    <div>
+      <div class="row justify-center">
+      <q-icon name="emoji_events" size="100px" />
+      </div>
+      <div class="row">
+        <h3>Felicitaciones hiciste x puntos</h3>
+      </div>
+      <div class="row justify-center q-gutter-md">
+        <q-btn  color="black" label="Volver a jugar" to="/" />
+        <q-btn  color="black" label="Compartir" @click="jugar()" />
+      </div>
+    </div>
+  </q-page>
 </template>
+
 <script>
 export default {
 
-}
+};
 </script>
